09ef77a920be79ce451dca1dacba89de473c617c
unknown <js@flockofbirds.nl>
2016-04-14 16:27:05 +0300

    Another method

--- UniteEDTeacher/UniteEDTeacher/Program.cs
+++ UniteEDTeacher/UniteEDTeacher/Program.cs
@@ -15,9 +15,10 @@ namespace UniteEDTeacher
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+        
         static void Main()
         {
-
+            
             Cef.Initialize(new CefSettings());
 
             Application.EnableVisualStyles();
@@ -33,13 +34,46 @@ namespace UniteEDTeacher
             }
 
         }
-
         private async static void checkForUpdates()
         {
-            using (var mgr = new UpdateManager("C:\\Projects\\MyApp\\Releases"))
+            using (var mgr = UpdateManager.GitHubUpdateManager("https://github.com/myuser/myapp"))
             {
-                await mgr.UpdateApp();
-            } 
+                await mgr.Result.UpdateApp();
+            }
+                
         }
+        /*private async static void CheckAndApplyUpdates()
+        {
+            string UpdatePath = "";
+            try
+            {
+                bool shouldRestart = false;
+                using (var mgr = new UpdateManager(UpdatePath))
+                {
+                    var updateInfo = mgr.CheckForUpdate().Result;
+                    if (updateInfo.CurrentlyInstalledVersion.Version < updateInfo.FutureReleaseEntry.Version)
+                    {
+                        shouldRestart = true;
+                        mgr.DownloadReleases(updateInfo.ReleasesToApply).Wait();
+                        mgr.ApplyReleases(updateInfo).Wait();
+                        await mgr.UpdateApp();
+                    }
+                }
+
+                if (shouldRestart)
+                {
+                    UpdateManager.RestartApp();
+                }
+            }
+            catch (Exception e)
+            {
+                // Uh Oh!
+            }
+        }*/
+
+        //private static bool CanUpdate()
+        //{
+        //    return !string.IsNullOrWhiteSpace(UpdatePath);
+        //}
     }
 }
